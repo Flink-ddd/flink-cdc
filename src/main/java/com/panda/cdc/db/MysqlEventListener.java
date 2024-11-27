940403ad8401cd7c02866aaa9c2972a6820c18de
vensenmu <vensenmu@gmail.com>
2024-11-27 11:44:43 +0800

    feat: optimization capture data change core function that mysql database to support

--- src/main/java/com/panda/cdc/db/MysqlEventListener.java
+++ src/main/java/com/panda/cdc/db/MysqlEventListener.java
@@ -24,6 +24,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.pulsar.FlinkPulsarSink;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -51,6 +52,9 @@ public class MysqlEventListener implements ApplicationRunner {
         this.dataChangeSink = dataChangeSink;
     }
 
+    @Value("${spring.cloud.stream.kafka.binder.brokers}")
+    private String brokers;
+
     @Override
     public void run(ApplicationArguments args) {
         CompletableFuture.runAsync(() -> {
@@ -60,10 +64,30 @@ public class MysqlEventListener implements ApplicationRunner {
             MySqlSource<DataChangeInfo> mySqlSource = buildDataChangeSource();
             DataStream<DataChangeInfo> streamSource = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "mysql-source").setParallelism(1);
             streamSource.addSink(dataChangeSink);
+
+            //构造第二个变更数据源（页面日志输出），仅作为日志输出。
+            MySqlSource<DataChangeInfo> mySqlSource2 = buildDataChangeSourceTwo();
+            DataStream<DataChangeInfo> streamSource2 = env.fromSource(mySqlSource2, WatermarkStrategy.noWatermarks(), "mysql-source").setParallelism(1);
+            streamSource2.addSink(dataChangeSink);
+
+            //构造第一个变更数据源
             //构造变更数据源（推送至业务kafka）123.57.204.93:9092,47.94.136.7:9092
             MySqlSource<String> debeziumSourceFunction = buildDataChangeSourceToString();
             DataStreamSource<String> stringDataStreamSource = env.fromSource(debeziumSourceFunction, WatermarkStrategy.noWatermarks(), "mysql-source").setParallelism(1);
-            stringDataStreamSource.addSink(getKafkaProducer("127.0.0.1:9092", "change-data"));
+
+            //构造第二个变更数据源
+            MySqlSource<String> debeziumSourceFunctionTwo = buildDataChangeSourceToStringTwo();
+            DataStreamSource<String> stringDataStreamSourceTwo = env.fromSource(debeziumSourceFunctionTwo, WatermarkStrategy.noWatermarks(), "mysql-source").setParallelism(1);
+
+            //进行数据源合流
+            DataStream<String> unionStringData = stringDataStreamSource.union(stringDataStreamSourceTwo);
+
+            //解析流数据，得到topic名称
+            DataStream<String> fieldNameStream = unionStringData.map(new MyMapFunction());
+
+            // 动态创建 Kafka Sink 并发送数据
+            fieldNameStream.map(new DynamicKafkaSink(brokers));
+
             try {
                 env.execute("mysql-stream-cdc");
             } catch (Exception e) {
@@ -73,6 +97,23 @@ public class MysqlEventListener implements ApplicationRunner {
             ex.printStackTrace();
             return null;
         });
+    }
+
+    private MySqlSource<DataChangeInfo> buildDataChangeSourceTwo() {
+        return MySqlSource.<DataChangeInfo>builder().hostname("127.0.0.1").port(3306).databaseList("order_center", "pay_center")
+                // 支持正则匹配
+                .tableList("^(order_center.m_order_address_info)", "^(pay_center.m_pay).*")
+                .username("root").password("625211234")
+                /**
+                 * initial初始化快照，即全量导入后增量导入(检测更新数据写入)
+                 * latest:只进行增量导入(不读取历史变化)
+                 * timestamp:指定时间戳进行数据导入(大于等于指定时间错读取数据)
+                 * 注：点击查看源码发现目前只支持initial以及latest了
+                 */
+//                .startupOptions(StartupOptions.initial())
+                .startupOptions(StartupOptions.latest())
+                //自定义序列化格式
+                .deserializer(new DataBaseDeserialization()).serverTimeZone("GMT+8").build();
     }
 
     /**
@@ -98,6 +139,24 @@ public class MysqlEventListener implements ApplicationRunner {
                 .startupOptions(StartupOptions.latest())
                 //自定义序列化格式
                 .deserializer(new DataBaseDeserialization()).serverTimeZone("GMT+8").build();
+    }
+
+    /**
+     * 构造变更数据源（推送至业务kafka或者pulsar）
+     *
+     * @param
+     * @return MySqlSource<String>
+     * @author muxh
+     */
+    private MySqlSource<String> buildDataChangeSourceToStringTwo() {
+        //可以从配置中读取
+        return MySqlSource.<String>builder().hostname("127.0.0.1").port(3306).databaseList("order_center", "pay_center")
+                // 支持正则匹配
+                .tableList("^(order_center.m_order_address_info)", "^(pay_center.m_pay).*")
+                // .tableList("order_center.m_order")
+                .username("root").password("625211234").startupOptions(StartupOptions.latest())
+                //自定义序列化格式
+                .deserializer(new CustomerDeserialization()).build();
     }
 
     /**
